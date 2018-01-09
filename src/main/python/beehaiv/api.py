87538326a94927e1c2e3efaf461afdfd46827bdc
Ingo Fruend <mail@ingofruend.net>
2018-01-09 18:08:26 -0500

    adding cors

--- src/main/python/beehaiv/api.py
+++ src/main/python/beehaiv/api.py
@@ -10,6 +10,27 @@ token_auth = hug.http(requires=hug.authentication.token(crypto.verify_token))
 admin_auth = hug.http(requires=hug.authentication.token(crypto.verify_admin))
 
 
+@hug.response_middleware()
+def CORS(request, response, resource):
+    response.set_header('Access-Control-Allow-Origin', '*')
+    response.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    response.set_header(
+        'Access-Control-Allow-Headers',
+        'Authorization,Keep-Alive,User-Agent,'
+        'If-Modified-Since,Cache-Control,Content-Type'
+    )
+    response.set_header(
+        'Access-Control-Expose-Headers',
+        'Authorization,Keep-Alive,User-Agent,'
+        'If-Modified-Since,Cache-Control,Content-Type'
+    )
+    if request.method == 'OPTIONS':
+        response.set_header('Access-Control-Max-Age', '1728000')
+        response.set_header('Content-Type', 'text/plain charset=UTF-8')
+        response.set_header('Content-Length', 0)
+        response.status_code = hug.HTTP_204
+
+
 # End point /experiments/
 @admin_auth.get('/experiments/')
 def get_all_experiments():
