52b917bec25204e4c3d93583ded66ad400cebf82
Ingo Fruend <mail@ingofruend.net>
2018-01-11 15:29:42 -0500

    exception handling centralized

--- src/main/python/beehaiv/api.py
+++ src/main/python/beehaiv/api.py
@@ -15,6 +15,10 @@ admin_auth = hug.http(requires=hug.authentication.token(crypto.verify_admin))
 def handle_exceptions(exception):
     if isinstance(exception, orm.ObjectNotFound):
         raise falcon.HTTPNotFound()
+    elif isinstance(exception, orm.TransactionIntegrityError):
+        raise falcon.HTTPConflict()
+    elif isinstance(exception, KeyError):
+        raise falcon.HTTPBadRequest()
     else:
         raise exception
 
@@ -52,23 +56,18 @@ def get_all_experiments():
 @admin_auth.get('/experiments/{exp_id}/', versions=1)
 def get_experiments(exp_id: int, response):
     with orm.db_session():
-        try:
-            return Experiment[exp_id].summary()
-        except orm.ObjectNotFound:
-            response.status = falcon.HTTP_404
+        return Experiment[exp_id].summary()
 
 
 @admin_auth.post('/experiments/', versions=1)
 def post_experiments(body, response, user: hug.directives.user):
     with orm.db_session():
         owner = User.get(username=user)
-        try:
-            expr = Experiment(owner=owner,
-                              name=body['name'],
-                              variable_names=body['variable_names'])
-        except KeyError:
-            response.status = falcon.HTTP_400
-            return
+        if body is None or 'name' not in body or 'variable_names' not in body:
+            raise falcon.HTTPBadRequest()
+        expr = Experiment(owner=owner,
+                          name=body['name'],
+                          variable_names=body['variable_names'])
     with orm.db_session():
         return expr.summary()
 
@@ -76,11 +75,7 @@ def post_experiments(body, response, user: hug.directives.user):
 @admin_auth.put('/experiments/{exp_id}/', versions=1)
 def put_experiments(exp_id: int, body, response):
     with orm.db_session():
-        try:
-            expr = Experiment[exp_id]
-        except orm.ObjectNotFound:
-            response.status = falcon.HTTP_404
-            return
+        expr = Experiment[exp_id]
         if 'name' in body:
             expr.name = body['name']
         if 'owner' in body:
@@ -92,11 +87,7 @@ def put_experiments(exp_id: int, body, response):
 @admin_auth.get('/experiments/{exp_id}/trials/', versions=1)
 def get_all_experiments_trials(exp_id: int, response, request):
     with orm.db_session():
-        try:
-            expr = Experiment[exp_id]
-        except orm.ObjectNotFound:
-            response.status = falcon.HTTP_404
-            return
+        expr = Experiment[exp_id]
         trials = (trial.summary() for trial in expr.trials)
         return json.dumps(list(trials))
 
@@ -106,24 +97,18 @@ def post_experiments_trials(exp_id: int,
                             body,
                             response,
                             user: hug.directives.user):
-    with orm.db_session():
-        try:
-            expr = Experiment[exp_id]
-        except orm.ObjectNotFound:
-            response.status = falcon.HTTP_404
-            return
+    if body is None:
+        raise falcon.BadRequest()
 
-        try:
-            observer = User.get(username=user)
-            trial_data = ','.join([str(body.pop(key))
-                                   for key in expr.variable_names.split(',')])
-        except KeyError:
-            response.status = falcon.HTTP_400
-            return
+    with orm.db_session():
+        expr = Experiment[exp_id]
+
+        observer = User.get(username=user)
+        trial_data = ','.join([str(body.pop(key))
+                               for key in expr.variable_names.split(',')])
 
         if len(body):
-            response.status = falcon.HTTP_400
-            return
+            raise falcon.HTTPBadRequest()
 
         trial = expr.trials.create(observer=observer, trial_data=trial_data)
 
@@ -134,32 +119,24 @@ def post_experiments_trials(exp_id: int,
 @admin_auth.get('/experiments/{exp_id}/trials/{trial_id}/', versions=1)
 def get_experiments_trials(exp_id: int, trial_id: int, response):
     with orm.db_session():
-        try:
-            expr = Experiment[exp_id]
-            trial = Trial[trial_id]
-        except orm.ObjectNotFound:
-            response.status = falcon.HTTP_404
-            return
+        expr = Experiment[exp_id]
+        trial = Trial[trial_id]
         if trial in expr.trials:
             return trial.summary()
         else:
-            response.status = falcon.HTTP_404
+            raise falcon.HTTPNotFound()
 
 
 @basic_auth.get('/experiments/{exp_id}/state/', versions=1)
 def get_state(exp_id: int, response, user: hug.directives.user):
     with orm.db_session():
-        try:
-            observer = User.get(username=user)
-            experiment = Experiment[exp_id]
-            state = State.get(observer=observer, experiment=experiment)
-            if state:
-                return state.state_json
-            else:
-                raise falcon.HTTPNotFound()
-        except orm.ObjectNotFound:
-            response.status = falcon.HTTP_404
-            return
+        observer = User.get(username=user)
+        experiment = Experiment[exp_id]
+        state = State.get(observer=observer, experiment=experiment)
+        if state:
+            return state.state_json
+        else:
+            raise falcon.HTTPNotFound()
 
 
 @basic_auth.post('/experiments/{exp_id}/state/', versions=1)
@@ -193,13 +170,9 @@ def put_state(exp_id: int, body, response, user: hug.directives.user):
 # End point /users/
 @hug.post('/users/', versions=1)
 def post_users(body, response):
-    try:
-        with orm.db_session():
-            user = User(username=body['username'], password=body['password'])
-        return user.safe_json()
-    except orm.TransactionIntegrityError:
-        response.status = falcon.HTTP_409
-        return
+    with orm.db_session():
+        user = User(username=body['username'], password=body['password'])
+    return user.safe_json()
 
 
 @admin_auth.get('/users/', versions=1)
@@ -211,24 +184,17 @@ def get_all_users():
 @token_auth.put('/users/{user_id}/', versions=1)
 def put_users(user_id: int, body, response, user: hug.directives.user):
     with orm.db_session():
-        try:
-            active_user = User.get(username=user)
-            user_ = User[user_id]
-        except orm.ObjectNotFound:
-            response.status = falcon.HTTP_404
-            return
+        active_user = User.get(username=user)
+        user_ = User[user_id]
 
         if not check_admin_privileges(user, user_):
-            response.status = falcon.HTTP_401
-            return
+            raise falcon.HTTPUnauthorized()
 
         if not set(body.keys()).issubset({'username', 'password', 'isadmin'}):
-            response.status = falcon.HTTP_400
-            return
+            raise falcon.HTTPBadRequest()
 
         if 'isadmin' in body and not active_user.isadmin:
-            response.status = falcon.HTTP_401
-            return
+            raise falcon.HTTPUnauthorized()
 
         for key, value in body.items():
             setattr(user_, key, value)
@@ -238,14 +204,10 @@ def put_users(user_id: int, body, response, user: hug.directives.user):
 @token_auth.get('/users/{user_id}/', versions=1)
 def get_users(user_id: int, response, user: hug.directives.user):
     with orm.db_session():
-        try:
-            user_ = User[user_id]
-        except orm.ObjectNotFound:
-            response.status = falcon.HTTP_404
-            return
+        user_ = User[user_id]
         if check_admin_privileges(user, user_):
             return user_.safe_json()
-        response.status = falcon.HTTP_401
+        raise falcon.HTTPUnauthorized()
 
 
 @basic_auth.get('/token/', versions=1)
